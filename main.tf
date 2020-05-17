396b8f67677225505e6d467facb5abcae3283e90
gautam254g <55299307+gautam254g@users.noreply.github.com>
2020-05-17 17:38:23 -0400

    Remove deprecated syntax

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ provider "aws" {
  }
 resource "aws_db_instance" "default" {
  ## depends_on             = ["aws_security_group.default"]
-  identifier             = "${var.identifier}"
+  identifier             = "var.identifier"
   allocated_storage      =  var.allocated_storage
   license_model          =  var.license_model
     storage_type           =  var.storage_type
-  engine                 = "${var.engine}"
+  engine                 = "var.engine"
   engine_version         = var.engine_version
-  instance_class         = "${var.instance_class}"
-  name                   = "${var.db_name}"
-  username               = "${var.username}"
+  instance_class         = "var.instance_class"
+  name                   = "var.db_name"
+  username               = "var.username"
   password               =  "${file("../rdspw/pwfile.txt")}"
  ## vpc_security_group_ids = ["${aws_security_group.default.id}"]
  ## db_subnet_group_name   = "${aws_db_subnet_group.default.id}"
