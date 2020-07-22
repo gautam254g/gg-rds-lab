3b94bfa37f778707a2467ea8a8e470d58ae3a505
gautam254g <55299307+gautam254g@users.noreply.github.com>
2020-07-22 19:07:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,16 +14,24 @@ provider "aws" {
   region  = "us-east-1"
 }
 resource "aws_db_instance" "default" {
-   identifier             = var.identifier
-  allocated_storage      =  var.allocated_storage
-  license_model          =  var.license_model
-  storage_type           =  var.storage_type
-  engine                 = var.engine
-  engine_version         = var.engine_version
-  instance_class         = var.instance_class
-  name                   = var.db_name
-  username               = var.username
- ## password               =  file("../rdspw/pwfile.txt")
-  password                 = "temp123"
-  skip_final_snapshot = true
+   identifier                          = "rdsgglpf11"
+  apply_immediately                   = true
+  engine                              = "oracle-se2"
+  engine_version                      = "12.2.0.1.ru-2020-01.rur-2020-01.r1"
+  license_model                       = "bring-your-own-license"
+  storage_type                        = "gp2"
+  allocated_storage                   =  25
+  instance_class                      = "db.t3.small"
+  db_name                              = "GGLPF11"
+  username                            = "RDSSYSTEM"
+  port                                = 1521
+  az                                  = "us-east-1b"
+  multi_az                            = false
+  parameter_group_name                = "GG-oracle-se2-12-2-par-group"
+  option_group_name                   = "GG-oracle-se2-12-2-opt-group"
+  maintenance_window                  = "Sun:02:00-Sun:06:00"
+  deletion_protection                 = false
+  backup_retention_period             = 1
+  cpm_backup                          = ""
+ snapshot_identifier                 = ""
 }
