6a4ee53adfe2112e247780aa5ba957bbf0812255
gautam254g <55299307+gautam254g@users.noreply.github.com>
2020-07-22 20:26:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
-  shared_credentials_file = "c/Users/gouta/.aws/credentials"
+  shared_credentials_file = "/c/Users/gouta/.aws/credentials"
 }
 resource "aws_instance" "web_server" {
  ami = "ami-b374d5a5"
