9fefb9d63a78b2a4114d76e467f4938f42da77ac
gautam254g <55299307+gautam254g@users.noreply.github.com>
2020-07-22 20:10:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,29 +13,7 @@ provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
 }
-resource "aws_db_instance" "test-rds" {
-   identifier                          = "rdsgglpf11"
-  apply_immediately                   = true
-  engine                              = "oracle-se2"
-  engine_version                      = "12.2.0.1.ru-2020-01.rur-2020-01.r1"
-  license_model                       = "bring-your-own-license"
-  storage_type                        = "gp2"
-  allocated_storage                   =  25
-  instance_class                      = "db.t3.small"
-   name                              = "GGLPF11"
-  username                            = "RDSSYSTEM"
-  port                                = 1521
-  identifier                          = "rdsgglpf11"
-  apply_immediately                   = true
-  engine                              = "oracle-se2"
-  engine_version                      = "12.2.0.1.ru-2020-01.rur-2020-01.r1"
-  license_model                       = "bring-your-own-license"
-  storage_type                        = "gp2"
-  allocated_storage                   =  25
-   multi_az                            = false
-  parameter_group_name                = "GG-oracle-se2-12-2-par-group"
-  option_group_name                   = "GG-oracle-se2-12-2-opt-group"
-  maintenance_window                  = "Sun:02:00-Sun:06:00"
-  deletion_protection                 = false
-  backup_retention_period             = 1
-  }
+resource "aws_instance" "web_server" {
+ ami = "ami-b374d5a5"
+ instance_type = "t2.micro"
+} 
