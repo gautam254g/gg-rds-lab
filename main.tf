15b70fec8edd23c7bfbc626a71ef758b05481a52
gautam254g <55299307+gautam254g@users.noreply.github.com>
2020-07-23 20:16:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,11 @@ terraform {
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::425518740947:user/gghosh"
+    session_name = "terraform"
+  }
 }
 resource "aws_instance" "web_server" {
  ami = "ami-b374d5a5"
