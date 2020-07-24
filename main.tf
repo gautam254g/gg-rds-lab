43ce3eb3c672fbd9438a763d42aa41d07c6c5f1b
gautam254g <55299307+gautam254g@users.noreply.github.com>
2020-07-23 20:30:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,10 @@
-terraform {
-  backend "remote" { 
-  hostname = "app.terraform.io"
-  organization = "gg-labs"
-  
-  workspaces  {
-    name = "gg-rds-lab"
-  }
- }
-}
+terraform{
+    backend s3 {
+        bucket = "gg-tfstate"
+        key    = "gg-remote.tfstate"
+        region = "us-east-1"
+        }
 
-provider "aws" {
-  version = "~> 2.0"
-  region  = "us-east-1"
-
-  assume_role {
-    role_arn = "arn:aws:iam::425518740947:user/gghosh"
-    session_name = "terraform"
-  }
 }
 resource "aws_instance" "web_server" {
  ami = "ami-b374d5a5"
