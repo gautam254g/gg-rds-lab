bc1e8f7d87ac0d3b1123bb9631a530d1dca19017
gautam254g <55299307+gautam254g@users.noreply.github.com>
2020-08-25 22:06:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "Web_server" {
 resource "aws_db_instance" "mydb1" {
   allocated_storage        = 256 # gigabytes
   backup_retention_period  = 7   # in days
-  db_subnet_group_name     = "${var.rds_public_subnet_group}"
+  ##db_subnet_group_name     = "${var.rds_public_subnet_group}"
   engine                   = "postgres"
   engine_version           = "9.5.4"
   identifier               = "mydb1"
@@ -35,5 +35,5 @@ resource "aws_db_instance" "mydb1" {
   storage_encrypted        = true # you should always do this
   storage_type             = "gp2"
   username                 = "mydb1"
-  vpc_security_group_ids   = ["${aws_security_group.mydb1.id}"]
+  ##vpc_security_group_ids   = ["${aws_security_group.mydb1.id}"]
 }
